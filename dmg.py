b5f36551c7b32989bf52b5edea3394fb456c7791
fractalbach <fractalbach@gmail.com>
2020-01-01 00:22:32 -0800

    updated

--- dmg.py
+++ dmg.py
@@ -6,20 +6,29 @@ import random
 class Player:
    
     MAX_ATTACK = 100
+    MAX_STRENGTH = 100
 
     def __init__(self, attack=1, strength=1, defense=1):
+        self.baseHitpoints = 100
         self.hp = 100
         self.attack = attack
         self.strength = strength
         self.defense = defense
     
-    def hit():
-        multiplier = (2 * MAX_ATTACK / self.attack) + random.random()
-        multiplier = math.max(multiplier, 1)
-        damage = multiplier * self.strength
+    def hit(self):
+        multiplier = (self.attack / self.MAX_ATTACK) + random.random()
+        multiplier = min(multiplier, 1)
+        effectiveStrength = (30 + self.strength) / (self.MAX_STRENGTH) * random.random()
+        damage = multiplier * effectiveStrength * 10
+        return damage
         
-    def isAlive():
-        return (self.hp <= 0)
+    def isAlive(self):
+        return (self.hp > 0)
+        
+    def __repr__(self):
+        return f"HP={self.hp}, ATK={self.attack}, STR={self.strength}, DEF={self.defense}"
+
+
 
 
 class Duel:
@@ -27,33 +36,84 @@ class Duel:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+        self.silent_mode = False
        
-    def displayHitpoints():
-        hp1 = math.round(self.player1, 2)
-        hp2 = math.round(self.player2, 2)
-        print(f"[HITPOINTS] p1: {hp1}, p2: {hp2}")
+    def displayHitpoints(self):
+        hp1 = self.player1.hp
+        hp2 = self.player2.hp
+        print(
+            f"[HITPOINTS]"
+            f"PLAYER1: {self.player1.hp:5.2f},  "
+            f"PLAYER2: {self.player2.hp:5.2f}"
+        )
         
-    def step():
-        dmg1 = self.player1.hit()
-        dmg2 = self.player2.hit()
-        print(f"p1 hits p2 for {math.round(dmg1,2)} damage!")
-        print(f"p2 hits p1 for {math.round(dmg2,2)} damage!")
-        p2.hp -= p1.hp
-        p1.hp -= p2.hp
-        self.displayHitpoints()
+    def displayStats(self):
+        print(f"[STATS][PLAYER 1]: {player1}")
+        print(f"[STATS][PLAYER 2]: {player2}")
+    
+    def displayDefeatMessage(self):
+        if not self.player1.isAlive():
+            print("player 1 has been defeated!")
+        if not self.player2.isAlive():
+            print("player 2 has been defeated!")   
+    
+    def step(self):
+        p1 = self.player1
+        p2 = self.player2
+        dmg1 = p1.hit()
+        dmg2 = p2.hit()
+        p2.hp -= dmg1
+        p1.hp -= dmg2
         
-    def run():
-        
+    def run(self):
+    
+        if not self.silent_mode: 
+            print("___!!!  D U E L   B E G I N S   !!!___")
+            self.displayStats()
+            
+        while self.player1.isAlive() and self.player2.isAlive():
+            self.step()
+            if not self.silent_mode: 
+                self.displayHitpoints()
+                
+        if not self.silent_mode: 
+            self.displayDefeatMessage()
+
+    
     
 
 if __name__ == "__main__":
-    player1 = Player(10, 10, 1)
-    player2 = Player(20, 20, 1)
+    player1 = Player(15, 5, 1)
+    player2 = Player(5, 10, 1)
+    record = {
+        1: 0,
+        2: 0,
+        "draws": 0,
+    }
     
-    print("battle begins!")
-    
-    while player1.isAlive() and player2.isAlive():
+    for _ in range(1000):
+        player1.hp = 10
+        player2.hp = 10
+        duel = Duel(player1, player2)
+        duel.silent_mode = True
+        duel.run()
+        if player1.isAlive():
+            record[1] += 1
+        elif player2.isAlive():
+            record[2] += 1
+        else:
+            record["draws"] += 1
+        
+    print(
+        f"________________________________________________\n"
+        f"================ DUEL RECORD ===================\n"
+        f"________________________________________________\n"
+        f"PLAYER 1 VICTORIES  : {record[1]}\n"
+        f"PLAYER 2 VICTORIES  : {record[2]}\n"
+        f"DUELS ENDED IN DRAW : {record['draws']}"
+    )
+
 
         
     
-    
+        
